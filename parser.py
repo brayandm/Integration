5b1eab978ed3646e50e5253b146360b4fba2c3db
brayandm <brayanduranmedina@gmail.com>
2022-07-04 02:36:56 -0400

    0 when it comes big or small

--- parser.py
+++ parser.py
@@ -2,6 +2,6 @@ def parse(expression_string):
 
     file = open('function.py', 'w')
 
-    file.write('from math import *\n\nfrom errors import *\n\ndef function(x):\n\n\tx = float(x)\n\n\ttry:\n\n\t\treturn ' + expression_string + '\n\n\texcept OverflowError:\n\n\t\treturn float(\'inf\')\n\n\texcept NameError:\n\n\t\tname_error()\n\n\t\treturn None\n\n\texcept:\n\n\t\treturn None')
+    file.write('from math import *\n\nfrom errors import *\n\ndef function(x):\n\n\tx = float(x)\n\n\ttry:\n\n\t\treturn ' + expression_string + '\n\n\texcept OverflowError:\n\n\t\treturn float(0)\n\n\texcept NameError:\n\n\t\tname_error()\n\n\t\treturn None\n\n\texcept:\n\n\t\treturn None')
 
     file.close()
